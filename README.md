f467a27bdc8bb0d335ce53fef3174cbc508e0bd2
Michal Cumpl <oswmcumpl@oriflame.com>
2022-10-01 22:00:56 +0200

    Update content

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # anideti
+
 Website of the Anideti animation school available at [www.anideti.cz](http://anideti.cz).
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  context: path.resolve(__dirname, './src'),
+  context: path.resolve(__dirname, "./src"),
   entry: {
-    app: './app.js',
+    app: "./app.js",
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
-    libraryTarget: 'var',
-    library: 'Lib'
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+    libraryTarget: "var",
+    library: "Lib",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: [{
-          loader: 'babel-loader',
-          options: { presets: ['es2015'] }
-        }],
-      }
-    ]
-  }
+        loader: [
+          {
+            loader: "babel-loader",
+            options: { presets: ["es2015"] },
+          },
+        ],
+      },
+    ],
+  },
 };
